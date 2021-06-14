083cc2c95703ce1094e6bf4167c3d83e597661c4
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-14 14:23:07 +0530

    changed test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hi this is Ramya"
+# Hi this is sai vamshi
 service_list=(kube-apiserver etcd kube-scheduler kube-controller-manager haproxy_apiserverlb kubelet kube-proxy docker-bootstrap docker)
 for i in "${service_list[@]}"
 do
